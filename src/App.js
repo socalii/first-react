d56014e648d1d308de8741cd3caea186e9cd82b3
Susan Choi <2539lee@gmail.com>
2022-04-18 23:32:37 -0700

    State

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
+import { useState } from 'react'
 import './App.css'
 import Box from './component/Box'
 
 function App() {
+  let counter = 0
+
+  const [counter2, setCounter2] = useState(0)
+
+  const increase = () => {
+    counter = counter + 1
+    setCounter2(counter2 + 1)
+  }
+
+  // 1. User clicked button
+  // 2. counter+1 => 1
+  // 3. called setState function
+  // 4. console.log executed
+  // counter = 1, counter2=> still not changed
+  // end increase()
+  // App re-render
+  // let counter=0, (reload)
+  // can see updated state
+
   return (
     <div>
-      <Box name='Lisa' num={1} />
-      <Box name='Jenny' num={2} />
-      <Box name='Jisoo' num={3} />
+      <div>{counter}</div>
+      <div>state: {counter2}</div>
+      <button onClick={increase}>Click!</button>
     </div>
   )
 }
