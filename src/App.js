78de8c908fb1b31b0476b9bd8a82588f0ce153ea
Susan Choi <2539lee@gmail.com>
2022-04-20 23:58:29 -0700

    React Lifecycle

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
 import Box from './component/Box'
 
@@ -6,10 +6,12 @@ function App() {
   let counter = 0
 
   const [counter2, setCounter2] = useState(0)
+  const [value, setValue] = useState(0)
 
   const increase = () => {
     counter = counter + 1
     setCounter2(counter2 + 1)
+    setValue(value + 2)
   }
 
   // 1. User clicked button
@@ -22,8 +24,17 @@ function App() {
   // let counter=0, (reload)
   // can see updated state
 
+  useEffect(() => {
+    console.log('useEffect1 Executed')
+  }, [])
+
+  useEffect(() => {
+    console.log('useEffect2 Executed', counter2, value)
+  }, [counter2, value])
+
   return (
     <div>
+      {console.log('render')}
       <div>{counter}</div>
       <div>state: {counter2}</div>
       <button onClick={increase}>Click!</button>
